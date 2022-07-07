42e40e5487b0f10d59d108c22d150345bddc79d8
Lucas Silva <lucascsilva.dev@gmail.com>
2022-07-06 22:24:51 -0300

    Initial structure created.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+export default function App() {
+  console.log("Test in browser's console.");
+
+  return (
+    <div>
+      <header>
+        <div className="bg-gray-100 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            Desafio Livraria IGTI
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>The content stay here.</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
