9588e52752dccca1de9ee60b8a520cc6dc6b66a3
Lucas Silva <lucascsilva.dev@gmail.com>
2022-07-07 21:00:05 -0300

    Header/Main components created.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 export default function App() {
-  console.log("Test in browser's console.");
-
   return (
-    <div>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Desafio Livraria IGTI
-          </h1>
-        </div>
-      </header>
+    <>
+      <Header>Bookstore</Header>
 
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>The content stay here.</h2>
-        </div>
-      </main>
-    </div>
+      <Main>The content is here!</Main>
+    </>
   );
 }
