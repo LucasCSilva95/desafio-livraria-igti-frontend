647d83109f5ec389897a8e5e0b67ea0493853607
Lucas Silva <lucascsilva.dev@gmail.com>
2022-07-10 20:57:05 -0300

    POST, PUT and Destroy created.

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,3 +4,18 @@ export async function get(url) {
   const { data } = await axios.get(url);
   return data;
 }
+
+export async function post(url, body) {
+  const { data } = await axios.post(url, body);
+  return data;
+}
+
+export async function put(url, body) {
+  const { data } = await axios.put(url, body);
+  return data;
+}
+
+export async function destroy(url) {
+  const { data } = await axios.delete(url);
+  return data;
+}
